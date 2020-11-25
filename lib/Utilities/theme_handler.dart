abed49aaa781e40876d889ba449e9dfd3e670e23
Shahzad Khan <shahzadqaisarkhan@gmail.com>
2020-11-26 03:24:38 +0500

    theme_handler

---
+++ lib/Utilities/theme_handler.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'my_theme_keys.dart';
+
+class _CustomTheme extends InheritedWidget {
+  final CustomThemeState data;
+
+  _CustomTheme({
+    this.data,
+    Key key,
+    @required Widget child,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(_CustomTheme oldWidget) {
+    return true;
+  }
+}
