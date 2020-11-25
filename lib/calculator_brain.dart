75361e8f03d7db8d713403fe63cb37646bc9c82c
Shahzad Khan <shahzadqaisarkhan@gmail.com>
2020-11-26 03:12:51 +0500

    lecture 56, in progress, ui of bmi

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CalculatorBrain {
+  final int height;
+  final int weight;
+
+  double _bmi;
+
+  CalculatorBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
