cded37f48c13e4c6c99514e04f7005d944e92477
Shahzad Khan <shahzadqaisarkhan@gmail.com>
2020-11-26 03:09:19 +0500

    list,heading,title

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,3 +11,21 @@ const kLabelTextStyle = TextStyle(
   fontSize: 20.0,
   color: Color(0xFF8D8E98),
 );
+const listHeading =
+TextStyle(fontSize: 18.0, color: Colors.grey, fontWeight: FontWeight.w900);
+
+const listTitle = TextStyle(
+    fontSize: 16.0, color: Colors.grey, fontWeight: FontWeight.normal);
+
+const listTrailing =
+TextStyle(fontSize: 14.0, color: Colors.grey, fontWeight: FontWeight.w900);
+
+const kNumberTextStyle = TextStyle(
+    color: Color.fromRGBO(86, 81, 104, 1),
+    fontSize: 50.0,
+    fontWeight: FontWeight.w900);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
